fe509b71680269b18eb9cd049a538a371a7c85b8
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-06-14 10:13:16 +0300

    Syntax, Functions and Statements - Lab

--- Syntax, Functions and Statements - Exercise/02. Greatest Common Divisor – GCD.js
+++
@@ -1,9 +0,0 @@
-function solve(a,b){
-  	for (let i = 1; i <= a && i <= b; i++) {
-    	if( a % i == 0 && b % i == 0) {
-      		divisor = i;
-    	}
-  	}
-  	console.log(divisor);
-}
-solve(15, 5);
