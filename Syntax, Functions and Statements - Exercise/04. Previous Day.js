ecc3bcaf4ab1711ec13b44622788c588bd7e1197
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-06-14 10:11:13 +0300

    Syntax, Functions and Statements - Lab

--- Syntax, Functions and Statements - Exercise/04. Previous Day.js
+++
@@ -1,7 +0,0 @@
-function solve(num1, num2, num3){
-    let dateInput = `${num1}-${num2}-${num3};`
-    let date = new Date(dateInput);
-    date.setDate(date.getDate() - 1);
-    console.log(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
-}
-solve(2016, 9, 30);
