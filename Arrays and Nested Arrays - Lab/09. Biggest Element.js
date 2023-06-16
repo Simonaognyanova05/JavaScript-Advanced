0ef859be0af01cdb4ccc91382045d92136a2aae3
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-06-16 21:40:17 +0300

    Arrays and Nested arrays - task 08 and 09

--- Arrays and Nested Arrays - Lab/09. Biggest Element.js
+++ Arrays and Nested Arrays - Lab/09. Biggest Element.js
@@ -1,3 +1,15 @@
-function solve(arr1, arr2){
+function solve(arr){
+    let bigArr = [];
+    let theBiggest1 = 0;
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr[i].length; j++){
+            bigArr.push(arr[i][j]);  
+        }
+    }
+    let sorted = bigArr.sort((a,b) => b - a);
+    theBiggest1 = sorted.shift();
+    console.log(theBiggest1)
 }
-solve([20, 40], [10, 60]);
+solve([[3, 5, 7, 12],
+    [-1, 4, 33, 2],
+    [8, 3, 0, 4]]);
