c00bd23aab013facf7539399fea264c8d49383bb
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-06-22 20:17:46 +0300

    03. Car Factory - redact

--- Objects and Composition - Exercise/03. Car Factory.js
+++ Objects and Composition - Exercise/03. Car Factory.js
@@ -1,11 +1,40 @@
 function solve(object) {
-    let newObject = {};
-    
+    let car = { model: '', engine: {}, carriage: {}, wheels: [] };
+    car.model = object.model;
+    car.engine.power = object.power;
+
+    if (car.engine.power <= 90) {
+        car.engine.power = 90;
+        car.engine.volume = 1800;
+    } else if (car.engine.power >= 90 && car.engine.power <= 120) {
+        ю
+        car.engine.power = 120;
+        car.engine.volume = 2400;
+    } else if (car.engine.power <= 200) {
+        car.engine.power = 200;
+
+        car.engine.volume = 3500;
+    }
+    car.carriage.type = object.carriage,
+        car.carriage.color = object.color;
+    if (object.wheelsize % 2 == 0) {
+        object.wheelsize -= 1;
+    } else {
+        object.wheelsize = object.wheelsize;
+    }
+    car.wheels.push(object.wheelsize);
+    car.wheels.push(object.wheelsize);
+    car.wheels.push(object.wheelsize);
+    car.wheels.push(object.wheelsize);
+
+    return car;
+
 }
-solve({
-    model: 'VW Golf II',
-    power: 90,
-    color: 'blue',
+let input = {
+    model: 'Brichka',
+    power: 65,
+    color: 'white',
     carriage: 'hatchback',
-    wheelsize: 14
-});
+    wheelsize: 16
+};
+console.log(solve(input));
