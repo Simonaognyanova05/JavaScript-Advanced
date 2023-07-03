3748d9c01c7bc9b77d7627107bc8db52382c5f31
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-07-03 14:33:53 +0300

    DOM Manipulation and Events - Lab

--- DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,12 @@
 function focused() {
-    
+    let inputElement = Array.from(document.getElementsByTagName('input'));
+
+    for (let input of inputElement) {
+        input.addEventListener('focus', (e) => {
+            e.target.parentNode.classList.add('focused');
+        })
+        input.addEventListener('blur', (e) => {
+            e.target.parentNode.classList.remove('focused');
+        })
+    }
 }
