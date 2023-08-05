09168e22f0b238d1886782d37c31ed047120a5fc
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-08-05 10:34:55 +0300

    Task 01

--- Exam Preparation - 2/01. Travel Agency/solution.js
+++ Exam Preparation - 2/01. Travel Agency/solution.js
@@ -1,80 +1,55 @@
 window.addEventListener('load', solution);
 
 function solution() {
-  let fullNameElement = document.getElementById('fname');
-  let emailElement = document.getElementById('email');
-  let phoneNumberElement = document.getElementById('phone');
-  let addressElement = document.getElementById('address');
-  let codeElement = document.getElementById('code');
+  const submitButton = document.getElementById('submitBTN');
+  const editButton = document.getElementById('editBTN');
+  const continueButton = document.getElementById('continueBTN');
+  const block = document.getElementById('block');
+  const preview = document.getElementById('infoPreview');
 
-  let submitBTNElement = document.getElementById('submitBTN');
-  let listElement = document.getElementById('infoPreview');
 
-  let editBTNElement = document.getElementById('editBTN');
-  let continueBTNElement = document.getElementById('continueBTN');
+  submitButton.disabled = false;
+  editButton.disabled = true;
+  continueButton.disabled = true;
 
-  let fullNameLiEl = document.createElement('li');
-  let emailLiEl = document.createElement('li');
-  let phoneNumberLiEl = document.createElement('li');
-  let addressLiEl = document.createElement('li');
-  let codeLiEl = document.createElement('li');
+  const inputValues = Array.from(document.getElementById('form').querySelectorAll('input'));
+  const labelValues = Array.from(document.getElementById('form').querySelectorAll('label'));
+  inputValues.pop();
 
-  let divElement = document.getElementById('block');
+  submitButton.addEventListener('click', (e) => {
+    const fullName = inputValues[0];
+    const email = inputValues[1];
 
-  submitBTNElement.addEventListener('click', (e) => {
-    if (fullNameElement.value && emailElement.value !== '') {
-      fullNameLiEl.textContent = `Full Name: ${fullNameElement.value}`;
-      listElement.appendChild(fullNameLiEl);
-
-      emailLiEl.textContent = `Email: ${emailElement.value}`;
-      listElement.appendChild(emailLiEl);
-
-      phoneNumberLiEl.textContent = `Phone Number: ${phoneNumberElement.value}`;
-      listElement.appendChild(phoneNumberLiEl);
-
-      addressLiEl.textContent = `Address: ${addressElement.value}`;
-      listElement.appendChild(addressLiEl);
-
-      codeLiEl.textContent = `Postal Code: ${codeElement.value}`;
-      listElement.appendChild(codeLiEl);
-
-      fullNameElement.value = '';
-      emailElement.value = '';
-      phoneNumberElement.value = '';
-      addressElement.value = '';
-      codeElement.value = '';
-      e.currentTarget.setAttribute('disabled', true);
-
-      editBTNElement.removeAttribute('disabled');
-      continueBTNElement.removeAttribute('disabled');
+    if (fullName.value && email.value !== '') {
+      for (let i = 0; i < inputValues.length; i++) {
+        const liElement = document.createElement('li');
+        liElement.innerText = `${labelValues[i].innerText} ${inputValues[i].value}`;
+        preview.appendChild(liElement)
+      }
+      for(let input of inputValues){
+        input.value = '';
+      }
+      e.currentTarget.disabled = true;
+      editButton.disabled = false;
+      continueButton.disabled = false;
     }
+
   })
 
-  editBTNElement.addEventListener('click', () => {
-    fullNameLiEl = fullNameLiEl.textContent.split(': ');
-    fullNameElement.value = fullNameLiEl[1];
-
-    emailLiEl = emailLiEl.textContent.split(': ');
-    emailElement.value = emailLiEl[1];
-
-    phoneNumberLiEl = phoneNumberLiEl.textContent.split(': ');
-    phoneNumberElement.value = phoneNumberLiEl[1];
-
-    addressLiEl = addressLiEl.textContent.split(': ');
-    addressElement.value = addressLiEl[1];
-
-    codeLiEl = codeLiEl.textContent.split(': ');
-    codeElement.value = codeLiEl[1];
-    listElement.remove();
-
-    editBTNElement.setAttribute('disabled', true);
-    continueBTNElement.setAttribute('disabled', true);
-    submitBTNElement.removeAttribute('disabled');
+  editButton.addEventListener('click', () => {
+    let listItems = Array.from(preview.childNodes);
+    for (let i = 0; i < inputValues.length; i++) {
+      inputValues[i].value = listItems[i].textContent.split(': ')[1];
+      listItems[i].remove();
+    }
+    submitButton.disabled = false;
+    editButton.disabled = true;
+    continueButton.disabled = true;
   })
-  continueBTNElement.addEventListener('click', () => {
-    divElement.innerHTML = '';
-    let hElement = document.createElement('h3');
-    hElement.textContent = 'Thank you for your reservation!'
-    divElement.appendChild(hElement);
+  continueButton.addEventListener('click', () => {
+    block.innerHTML = '';
+    let h3 = document.createElement('h3');
+    h3.textContent = 'Thank you for your reservation!';
+    block.appendChild(h3);
   })
 } 
