6c728f3a3b6dd5b95954decb2790f300e7e4353b
Simonaognyanova05 <simonaognanova05@gmail.com>
2023-08-11 14:51:12 +0300

    Add 'Game Over'

--- Workshop - Wizard Side Scrolling Game/src/js/gameEngine.js
+++ Workshop - Wizard Side Scrolling Game/src/js/gameEngine.js
@@ -31,6 +31,10 @@ function gameLoop(state, game, timestamp){
     bugElements.forEach(bug => {
         let posX = parseInt(bug.style.left);
 
+        //Detect collision
+        if(detectCollision(wizardElement, bug)){
+            state.gameOver = true;
+        }
         if(posX > 0){
             bug.style.left = posX - state.bugStats.speed + 'px';
         }else{
@@ -61,7 +65,11 @@ function gameLoop(state, game, timestamp){
     wizardElement.style.left = wizard.posX + 'px';
     wizardElement.style.top = wizard.posY + 'px';
     
-    window.requestAnimationFrame(gameLoop.bind(null, state, game));
+    if(state.gameOver){
+        alert('Game Over');
+    }else{
+        window.requestAnimationFrame(gameLoop.bind(null, state, game));
+    }
 
 }
 function modifyWizardPositions(state, game){
